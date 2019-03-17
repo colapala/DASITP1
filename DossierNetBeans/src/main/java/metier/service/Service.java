2e5f94d9b066492adc02545e1569ccbd40fcaaf6
LAHAROTTE corentin <colapala@gmail.com>
2019-03-17 18:51:35 +0100

    ajout des ressources moodle

--- DossierNetBeans/src/main/java/metier/service/Service.java
+++ DossierNetBeans/src/main/java/metier/service/Service.java
@@ -25,7 +25,7 @@ import metier.modele.Personne;
  */
 public class Service{
 
-	PersonneDaoJpa pdj;
+	/*PersonneDaoJpa pdj;
 	InterventionDaoJpa idj;
 
         //pas besoin de constructeur normalement
@@ -33,24 +33,23 @@ public class Service{
 	Service(){
 		pdj = new PersonneDaoJpa();
 		idj = new InterventionDaoJpa();
-	}
+	}*/
 
-	public boolean SeConnecter(String mail, String motdepasse){
-		Personne a = pdj.recupererPersonne(mail);
+	public static boolean SeConnecter(String mail, String motdepasse){
+		Personne a =PersonneDaoJpa.recupererPersonne(mail);
 		if (a!=null){
-			if (a.getMotDePasse()== motdepasse){ // a.getMotDePasse().equals(motdepasse)?
+			if (a.getMotDePasse().equals(motdepasse))//a.getMotDePasse()== motdepasse){ // a.getMotDePasse().equals(motdepasse)?
 				return true;
-			}
 		}
 		return false;
 	}
 
-	public boolean SeInscrire(boolean civilite, String nom, String prenom, String motDePasse, String adressePostale, String tel, String mail, Date dateDeNaissance){
-		Personne x = pdj.recupererPersonne(mail);
+	public static boolean SeInscrire(boolean civilite, String nom, String prenom, String motDePasse, String adressePostale, String tel, String mail, Date dateDeNaissance){
+		Personne x = PersonneDaoJpa.recupererPersonne(mail);
 		if (x==null){
 			if(8 < motDePasse.length() && motDePasse.length()< 16){
 				Client a = new Client (civilite, nom, prenom, motDePasse, adressePostale, tel, mail, dateDeNaissance);
-				pdj.creerPersonne(a);
+				PersonneDaoJpa.creerPersonne(a);
 				return true;
 			}
 			else{
@@ -67,71 +66,74 @@ public class Service{
 	// I have supposed you cannot have a intervetionanimal without an animal, because then it would be no diference to interventionIncident (sauf le type ;)
 	//the same with interventionlivraison
 	//actually, we dont even need the type but I'll leave it
-	public Intervention DemanderIntervention(int type, String description){
+	public static Intervention DemanderIntervention(int type, String description){
 			InterventionIncident a = new InterventionIncident(description);
-			idj.creerIntervention(a);
+			InterventionDaoJpa.creerIntervention(a);
 			return a;
 	}
 
-	public Intervention DemanderIntervention(int type, String description, String objet, String entreprise){
+	public static Intervention DemanderIntervention(int type, String description, String objet, String entreprise){
 			InterventionLivraison a = new InterventionLivraison(objet, entreprise, description);
-			idj.creerIntervention(a);
+			InterventionDaoJpa.creerIntervention(a);
 			return a;
 	}
 
-	public Intervention DemanderIntervention(int type, String description, String animal){
+	public static Intervention DemanderIntervention(int type, String description, String animal){
 			InterventionAnimal a = new InterventionAnimal(animal, description);
-			idj.creerIntervention(a);
+			InterventionDaoJpa.creerIntervention(a);
 			return a;
 	}
 	//Im guessing recuperListeIntervention could get the intervention of clients and employes
-	public List<Intervention> ConsulterHistorique(Personne p){
-		return idj.recupererListeIntervention(p);
+	public static List<Intervention> ConsulterHistorique(Personne p){
+		return InterventionDaoJpa.recupererListeIntervention(p);
 	}
 	
-	public void AfficherHistorique (List<Intervention> listint){
+	public static void AfficherHistorique (List<Intervention> listint){
 		for (Intervention i : listint){
 			AfficherIntervention(i);
 		}
 	}
 
-	public boolean CloturerIntervention(Employe emp, int status, int heureDefin, String commentaire){
+	public static boolean CloturerIntervention(Employe emp, int status, int heureDefin, String commentaire){
 		Intervention i = RechercherIntervetnionEnCours(emp);
 		i.setStatus(status);
 		i.setHeureDeFin(heureDefin);
 		i.setCommentaire(commentaire);
-		idj.modifierIntervention(i);
+		InterventionDaoJpa.modifierIntervention(i);
                 return true;
 	}
 
-	public Intervention RechercherIntervetnionEnCours(Employe emp){
-		return idj.recupererInterventionEnCours(emp);
+	public static Intervention RechercherIntervetnionEnCours(Employe emp){
+		return InterventionDaoJpa.recupererInterventionEnCours(emp);
 	}
 
-	public void AfficherIntervention( Intervention i){
+	public static void AfficherIntervention( Intervention i){
 		int type = i.getType();
 		String description = i.getDescription();
-		int status = i.getStatus(); //int ?
-                //on pourrait faire un switch, ce serait un peu mieux
-		if (type == 1){
-			System.out.println ("Intervention Animal");
-		}
-		else if(type == 2){
-			System.out.println ("Intervention Livraison");
-		}
-                else if (type==3){
-			System.out.println ("Intervention Incident");
-                }else{
+		int status = i.getStatus();
+            //on pourrait faire un switch, ce serait un peu mieux
+            switch (type) {
+                case 1:
+                    System.out.println ("Intervention Animal");
+                    break;
+                case 2:
+                    System.out.println ("Intervention Livraison");
+                    break;
+                case 3:
+                    System.out.println ("Intervention Incident");
+                    break;
+                default:
                     System.out.println ("Erreur, nombre invalide");
-		}
+                    break;
+            }
 		System.out.println("desc: " + description + "/n" + "status: " + status + "/n");
 	}
 
 
 	//main to check things
-	public static void main(String[] args){
-		System.out.println ("Pruebas");
-	}
+	/*public static void main(String[] args){
+		
+	}*/
 
 }
 
