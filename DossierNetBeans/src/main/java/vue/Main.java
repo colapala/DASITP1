1f41b7835ae584a5a1983f64823d7806c025dc06
LAHAROTTE corentin <colapala@gmail.com>
2019-03-18 18:09:08 +0100

    tp2

--- DossierNetBeans/src/main/java/vue/Main.java
+++ DossierNetBeans/src/main/java/vue/Main.java
@@ -7,6 +7,7 @@ package vue;
 
 import dao.InterventionDaoJpa;
 import dao.JpaUtil;
+import dao.PersonneDaoJpa;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -39,14 +40,16 @@ public class Main {
         Date d=new Date();
         Client p2 = new Client(false, "DELAPIERRE", "ROGER", "motdepasse", "15 Rue Ampère, Paris", "063256585", "mail1@mail.com",d);
         Client p1 = new Client(false,"ANDRE","Anouchka", "mdp", "15 Rue de la Paix, Paris", "0695666406", "googlemail@mail.com",d);
-        Employe E2 = new Employe(true, "DUCAILLOUX", "PIERRE", "present", "15 rue René, villeurbanne", "063256585", "mail2@mail.com",true,100,10);
+        Employe E2 = new Employe(true, "DUCAILLOUX", "PIERRE", "present", "15 rue René, villeurbanne", "063256585", "mail2@mail.com",false,100,10);
         InterventionAnimal A1=new InterventionAnimal("hot-dog","scoubidou");
         InterventionLivraison L1=new InterventionLivraison("colis","Entreprise","colis non livré");
         InterventionIncident I1=new InterventionIncident("j'ai un probleme");
-        //test des relations
-        I1.ajouterPersonne(p2,E2);
-        L1.ajouterPersonne(p1,E2);
         
+        //test des relations
+        p2.AjouterIntervention(I1);
+        p2.AjouterIntervention(A1);
+        E2.AjouterIntervention(L1);
+         
         EntityTransaction tx = em.getTransaction();
             tx.begin();
             em.persist(p2);
@@ -56,11 +59,11 @@ public class Main {
             em.persist(L1);
             em.persist(I1);
             tx.commit();
-            em.close();
-            
+        em.close();
+          //  emf.close();
     }
     
-    public static void AffichageMainMenu(){
+    public static void affichageMainMenu(){
         System.out.println("--------------------");
         System.out.println("Bienvenue sur ProAct'IF");
         System.out.println("--------------------");
@@ -70,26 +73,26 @@ public class Main {
         System.out.println();
     }
     
-    public static void AffichageMenuClient(){
+    public static void affichageMenuClient(){
         System.out.println("Choisir une option:");
         System.out.println("3.Demander une intervention");
         System.out.println("4.Consulter Historique");
         System.out.println();
     }
     
-     public static void AffichageMenuEmploye(){
+     public static void affichageMenuEmploye(){
         System.out.println("Choisir une option:");
         System.out.println("5.Cloturer l'intervention en cours");
         System.out.println("6.Consulter Tableau de bord");
         System.out.println();
     }
     
-    public static void AffichageOption(int option){
+    public static void affichageOption(int option){
     
     }
      
-    public static void LancerMenu(){
-         AffichageMainMenu();
+    public static void lancerMenu(){
+         affichageMainMenu();
          int choix = Saisie.lireInteger("Choix: ", Arrays.asList(1,2));
          switch(choix){
              case 1:
@@ -104,7 +107,8 @@ public class Main {
     
     
     public static void main(String[] args) {
-       // MainMenu();
+       //LancerMenu();
+       Personne p;
         
         //Test sur remplissage
         Remplir();
@@ -113,12 +117,11 @@ public class Main {
         JpaUtil.creerEntityManager();
         JpaUtil.ouvrirTransaction();
         
-        Personne trouve=Service.SeConnecter("mail1@mail.com","motdepasse");
-        System.out.println("Personne :"+trouve.getNom()+" "+trouve.getPrenom());
-           
-        //List<Intervention> result=InterventionDaoJpa.recupererListeIntervention(trouve);// -> fonctionne pas
-
-        //Service.AfficherHistorique(result);
+        //services testés
+        //-SeConnecter
+        //-SeInscrire
+        //-getListInterventions
+ 
         
         JpaUtil.validerTransaction();
         JpaUtil.fermerEntityManager();
