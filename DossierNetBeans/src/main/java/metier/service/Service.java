d9fe34b3a7e16bf1db5aa502d9bd921870ae4b94
terreu <43096121+terreu@users.noreply.github.com>
2019-03-18 15:39:55 +0100

    Update Service.java
    It has been added the right returns, null when it doesnt work, intervention/personne... when it does.

--- DossierNetBeans/src/main/java/metier/service/Service.java
+++ DossierNetBeans/src/main/java/metier/service/Service.java
@@ -16,6 +16,7 @@ import metier.modele.InterventionAnimal;
 import metier.modele.InterventionIncident;
 import metier.modele.InterventionLivraison;
 import metier.modele.Personne;
+import java.util.Date;
 
 
 /**
@@ -25,63 +26,64 @@ import metier.modele.Personne;
  */
 public class Service{
 
-	/*PersonneDaoJpa pdj;
-	InterventionDaoJpa idj;
-
-        //pas besoin de constructeur normalement
-	//Constructeur
-	Service(){
-		pdj = new PersonneDaoJpa();
-		idj = new InterventionDaoJpa();
-	}*/
 
 	public static Personne SeConnecter(String mail, String motdepasse){
+
 		Personne a=PersonneDaoJpa.recupererPersonne(mail);
 		if (a!=null){
-			if (a.getMotDePasse().equals(motdepasse))//a.getMotDePasse()== motdepasse){ 
+			if (a.getMotDePasse().equals(motdepasse)){//a.getMotDePasse()== motdepasse){ 
 				return a;
+			}
 		}
                 return null;
 	}
 
-	public static boolean SeInscrire(boolean civilite, String nom, String prenom, String motDePasse, String adressePostale, String tel, String mail, Date dateDeNaissance){
+	// On verifie ici que:
+	// nothing is null,
+	// mail contains @
+	// person is over 16 years old
+	public static Personne SeInscrire(boolean civilite, String nom, String prenom, String motDePasse, String adressePostale, String tel, String mail, Date dateDeNaissance){
 		Personne x = PersonneDaoJpa.recupererPersonne(mail);
 		if (x==null){
-			if(8 < motDePasse.length() && motDePasse.length()< 16){
+			Date ahorita = new Date();
+			if(8 < motDePasse.length() && motDePasse.length()< 16 && nom.lenght()>0 && prenom.lenght()>0 && mail.lenght()>2 && mail.contains("@") &&
+			adressePostale.lenght()>0 && tel.lenght()>0 && ((ahorita.getTime() - dateDeNaissance.getTime()) > 16*365*24*60*60*1000)) {
 				Client a = new Client (civilite, nom, prenom, motDePasse, adressePostale, tel, mail, dateDeNaissance);
 				PersonneDaoJpa.creerPersonne(a);
-				return true;
+				return a;
 			}
-			else{
-				System.out.println ("Password must be between 8 and 16 characters");
-			}
-
 		}
-		else{
-			System.out.println ("That mail is already used by someone.");
-		}
-		return false;
+		return null;
 	} 	
 
 	// I have supposed you cannot have a intervetionanimal without an animal, because then it would be no diference to interventionIncident (sauf le type ;)
 	//the same with interventionlivraison
 	//actually, we dont even need the type but I'll leave it
 	public static Intervention DemanderIntervention(int type, String description){
+		if (type>= 0 && type <=3 && description.lenght()>0){
 			InterventionIncident a = new InterventionIncident(description);
 			InterventionDaoJpa.creerIntervention(a);
-			return a;
+			return a;	
+		}
+		return null;
 	}
 
 	public static Intervention DemanderIntervention(int type, String description, String objet, String entreprise){
+		if (type>= 0 && type <=3 && description.lenght()>0 && objet.length()>0 && entreprise.length()>0){ 
 			InterventionLivraison a = new InterventionLivraison(objet, entreprise, description);
 			InterventionDaoJpa.creerIntervention(a);
 			return a;
+		}
+		return null;
 	}
 
 	public static Intervention DemanderIntervention(int type, String description, String animal){
+		if (type>= 0 && type <=3 && description.lenght()>0 && objet.length()>0 && entreprise.length()>0){
 			InterventionAnimal a = new InterventionAnimal(animal, description);
 			InterventionDaoJpa.creerIntervention(a);
 			return a;
+		}
+		return null;
 	}
 	//Im guessing recuperListeIntervention could get the intervention of clients and employes
 	public static List<Intervention> ConsulterHistorique(Personne p){
@@ -96,11 +98,14 @@ public class Service{
 
 	public static boolean CloturerIntervention(Employe emp, int status, int heureDefin, String commentaire){
 		Intervention i = RechercherIntervetnionEnCours(emp);
-		i.setStatus(status);
-		i.setHeureDeFin(heureDefin);
-		i.setCommentaire(commentaire);
-		InterventionDaoJpa.modifierIntervention(i);
-                return true;
+		if (status >= 0 && status <=3 && commentaire.lenght()>0 && heureDefin >= 0 && heureDefin < 24 && i != null){
+			i.setStatus(status);
+			i.setHeureDeFin(heureDefin);
+			i.setCommentaire(commentaire);
+			InterventionDaoJpa.modifierIntervention(i);
+			return true;
+		}
+		return false;
 	}
 
 	public static Intervention RechercherIntervetnionEnCours(Employe emp){
@@ -126,14 +131,12 @@ public class Service{
                     System.out.println ("Erreur, nombre invalide");
                     break;
             }
-		System.out.println("desc: " + description + "/n" + "status: " + status + "/n");
+		System.out.println("status: " + status + "/n" + "desc: " + description + "/n" );
 	}
 
 
 	//main to check things
-	/*public static void main(String[] args){
-		
-	}*/
-
+	public static void main(String[] args){
+		//System.out.println ("Pruebas");
+	}
 }
-
